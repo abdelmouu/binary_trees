7a6708c8bda770a8a05f7bffd2c900e4e6063da8
abdallah moumni <abdelrebel001@gmail.com>
2024-03-28 23:43:59 -0700

    commiting binary tree size and binary tree leaves

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -10,10 +10,5 @@ size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-
-	size_t size = 1;
-
-	size += binary_tree_size(tree->left);
-	size += binary_tree_size(tree->right);
-	return (size);
+	return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
 }
